6d7790d0c0e9d3d677c01e7991bda667b9727b6f
Lyb0416 <896216358@qq.com>
2024-09-12 22:46:37 +0800

    1、做了基础管理员界面

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { createApp } from 'vue'
-import './style/style.css'
 import App from './App.vue'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import app from "./App.vue";
 import router from "./router/router.ts";
 
 
-createApp(App).mount('#app')
-app.use(ElementPlus)
+const app = createApp(App)
 app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
 
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import rootIndex from "../components/RootIndex.vue";
+import Login from "../components/Login.vue";
+import Availability from "../views/Availability.vue";
+import Orders from "../views/Orders.vue";
+import Users from "../views/Users.vue";
+
 
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-
+        {
+            path:'/',
+            redirect: '/rootIndex'
+        },
+        {
+            path:'/rootIndex',
+            name:'rootIndex',
+            component:rootIndex,
+            children:[
+                {
+                    path:'availability',
+                    component:Availability
+                },
+                {
+                    path:'orders',
+                    component:Orders
+                },
+                {
+                    path:'users',
+                    component:Users
+                }
+            ]
+        },
+        {
+            path:'/login',
+            component: Login
+        }
     ]
 })
 
