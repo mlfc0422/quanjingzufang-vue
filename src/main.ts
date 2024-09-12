f16adb10e8f9c228d27e887da7b0f9b9658729ed
mlfc <mlfc0422@outlook.com>
2024-09-12 15:23:49 +0800

    加入依赖和vuerouter

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import app from "./App.vue";
+import router from "./router/router.ts";
+
 
 createApp(App).mount('#app')
+app.use(ElementPlus)
+app.use(router)
+
