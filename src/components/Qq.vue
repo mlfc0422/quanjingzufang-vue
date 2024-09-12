55b1ddbeff89f0a12fe41f5d8de3e3b2d21ee653
liyoutao <995289840@qq.com>
2024-09-12 15:30:52 +0800

    1

---
+++ src/components/Qq.vue
@@ -0,0 +1,7 @@
+<template>
+  hhhh
+</template>
+
+<script lang="ts" setup>
+
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+//
+// createApp(App).mount('#app')
+// 引入creatApp用于创建应用
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+//导入element-plus
+//整体导入 ElementPlus 组件库
+import ElementPlus from 'element-plus' //导入 ElementPlus 组件库的所有模块和功能
+import 'element-plus/dist/index.css' //导入 ElementPlus 组件库所需的全局 CSS 样式
+
+//引入App根组件
+import App from './App.vue'
+//引入路由器
+import router from './router'
+
+//创建一个应用
+const app = createApp(App)
+app.use(ElementPlus) //将 ElementPlus 插件注册到 Vue 应用中
+app.mount('#app')//使用路由器
+app.use(router)//挂载整个应用到app容器中
